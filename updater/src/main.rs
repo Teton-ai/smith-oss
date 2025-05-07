2c77b0da6df432cd64103a4533486c842a004794
Alvaro Molina <alw3ys@gmail.com>
2025-05-07 10:03:23 +0200

    feat: smith updater (#7)

---
+++ updater/src/main.rs
@@ -0,0 +1,92 @@
+use clap::Parser;
+use smith::actors::ShutdownHandler;
+use smith::actors::magic::MagicHandle;
+use tokio::time;
+use tracing::{error, info};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    Args::parse();
+    tracing_subscriber::fmt::init();
+    info!("Smith Updater Starting");
+
+    tokio::time::sleep(time::Duration::from_secs(30)).await;
+
+    info!("Smith Updater Updating");
+    let shutdown = ShutdownHandler::new();
+
+    let configuration = MagicHandle::new(shutdown.signals());
+
+    configuration.load(None).await;
+
+    time::sleep(time::Duration::from_secs(5)).await;
+
+    let packages = configuration.get_packages().await;
+
+    let smith_package = packages
+        .iter()
+        .filter(|package| package.name == "smith" || package.name == "smith_amd64")
+        .collect::<Vec<_>>();
+
+    assert_eq!(smith_package.len(), 1);
+
+    // check current version of smith
+    let output = tokio::process::Command::new("dpkg")
+        .arg("-l")
+        .arg("smith")
+        .output()
+        .await
+        .expect("Failed to execute dpkg command");
+
+    let package_file = smith_package[0].file.clone();
+    let package_version = smith_package[0].version.clone();
+    let mut package_installed = false;
+    if output.status.success() {
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        let lines: Vec<&str> = stdout.lines().collect();
+
+        // Assuming the package version is in the second column of the first line
+        if let Some(package_info) = lines.get(5) {
+            let fields: Vec<&str> = package_info.split_whitespace().collect();
+            if let Some(version) = fields.get(2) {
+                info!(
+                    "Package installed -> {version} | {} <- Magic Version",
+                    &package_version
+                );
+                package_installed = version == &package_version
+            }
+        }
+    };
+
+    if !package_installed {
+        info!("Package must already be available");
+        let package_location = format!("packages/{}", package_file);
+        info!("Installing package: smith");
+        let install_command = format!("sudo apt install ./{} -y", package_location);
+        let status = tokio::process::Command::new("sh")
+            .arg("-c")
+            .arg(install_command)
+            .output()
+            .await
+            .map_err(|e| {
+                error!("Failed to run command to install smith: {}", e);
+                e
+            })?;
+
+        if status.status.success() {
+            info!("Smith installed! Restarting");
+        } else {
+            error!("Failed to install smith");
+        }
+    } else {
+        info!("Package already installed");
+    }
+
+    info!("Smith Updater Shutting Down");
+
+    Ok(())
+}
